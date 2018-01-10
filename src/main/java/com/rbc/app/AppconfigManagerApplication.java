f5035f8d74794306299f203cd6d22d7472684181
Gerrard Lee <Gerrard.Lee@scotiabank.com>
2018-01-09 19:23:12 -0500

    Initial commit

---
+++ src/main/java/com/rbc/app/AppconfigManagerApplication.java
@@ -0,0 +1,12 @@
+package com.rbc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppconfigManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppconfigManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rbc/app/AppconfigManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbc.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppconfigManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
