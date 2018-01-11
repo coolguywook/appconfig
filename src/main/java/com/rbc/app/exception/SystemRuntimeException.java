7bd79556eadb6771cf2aeb2a55138899f48d1933
Gerrard Lee <Gerrard.Lee@scotiabank.com>
2018-01-11 18:31:56 -0500

    Add comment

--- src/main/java/com/rbc/app/exception/SystemRuntimeException.java
+++ src/main/java/com/rbc/app/exception/SystemRuntimeException.java
@@ -3,6 +3,9 @@ package com.rbc.app.exception;
 
 import com.google.common.base.Preconditions;
 
+/*
+ * [Generic Logic] User defined runtime exception. 
+ */
 public class SystemRuntimeException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/rbc/app/repository/VersionRepository.java
+++ src/main/java/com/rbc/app/repository/VersionRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rbc.app.domain.Version;
 import com.rbc.app.domain.VersionId;
 
+/*
+ * [Geleric Logic] Interface to db's operation which is inherited from JpaRepository.
+ * [Table] Version
+ */
 public interface VersionRepository extends JpaRepository<Version, VersionId> {
 
 }
